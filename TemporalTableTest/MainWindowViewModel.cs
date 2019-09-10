334f018a11c649c8f420d400a70a7d9effe0ce37
Akira Otaka <hydrangeas.project@gmail.com>
2019-09-10 23:38:18 +0900

    ユーザーコントロール指定用にVMを修正

--- TemporalTableTest/MainWindowViewModel.cs
+++ TemporalTableTest/MainWindowViewModel.cs
@@ -1,6 +1,39 @@
 ﻿namespace TemporalTableTest
 {
-    class MainWindowViewModel: ViewModelBase
+    public class MainWindowViewModel: ViewModelBase
     {
+        /// <summary>
+        /// 表示画面タイプ
+        /// </summary>
+        public enum ViewTypes : int
+        {
+            /// <summary>
+            /// 一覧表示画面
+            /// </summary>
+            PostList = 0,
+            /// <summary>
+            /// 新規、更新画面
+            /// </summary>
+            PostEdit,
+            /// <summary>
+            /// 履歴画面
+            /// </summary>
+            PostHistory
+        }
+
+        private ViewTypes _ViewType = ViewTypes.PostList;
+        /// <summary>
+        /// 表示画面のタイプ
+        /// </summary>
+        public ViewTypes ViewType
+        {
+            get => _ViewType;
+            set
+            {
+                if (value == _ViewType)
+                    return;
+                _ViewType = value;
+            }
+        }
     }
 }
