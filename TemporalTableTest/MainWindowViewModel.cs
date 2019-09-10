4263dde0b66460f6e22174aa10da2bffcf17a6c7
Akira Otaka <hydrangeas.project@gmail.com>
2019-09-10 23:03:45 +0900

    ViewModel追加
    * 基底クラスも追加

---
+++ TemporalTableTest/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TemporalTableTest
+{
+    class MainWindowViewModel: ViewModelBase
+    {
+    }
+}
---
+++ TemporalTableTest/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TemporalTableTest
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
